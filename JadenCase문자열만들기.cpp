9e6ad955866eb32df9c5ca00ff27fd41157b0cd4
Chan531 <chan657312@gmail.com>
2021-06-27 15:27:59 +0900

    프로그래머스 2단계 JadenCase 문자열 만들기

---
+++ JadenCase문자열만들기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    if (s[0] > 96 && s[0] < 123)
+    {
+        s[0] -= 32;
+    }
+    for (int i = 1; i < s.length(); i++)
+    {
+        if ((s[i] > 96 && s[i] < 123) && s[i - 1] == 32)
+        {
+            s[i] -= 32;
+        }
+        else if ((s[i] > 64 && s[i] < 91) && s[i - 1] != 32)
+        {
+            s[i] += 32;
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    solution("3people unFollowed me");
+}
